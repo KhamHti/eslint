013e3647cf4b5a576723b4a7ee32dabb1b97fe0b
Khun Kyaw Kham Hti <khamhti@agga.io>
2023-12-21 00:59:45 +0630

    asdf

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+// /* eslint-disable react-native/no-single-element-style-arrys */
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,13 +7,17 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+  // eslint-disable-next-line react-native/no-color-literals
   container: {
+
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
---
+++ README.md
@@ -0,0 +1,18 @@
+## dependencies
+```bash
+
+```
+
+## dev-dependencies
+```bash
+
+yarn add eslint --dev
+yarn add @babel/eslint-parser --dev
+yarn add eslint-config-airbnb --dev
+yarn add eslint-plugin-jsx-a11y --dev 
+yarn add eslint-plugin-import --dev
+yarn add eslint-plugin-react --dev
+yarn add eslint-plugin-react-hooks --dev
+yarn add eslint-plugin-react-native --dev
+
+```
